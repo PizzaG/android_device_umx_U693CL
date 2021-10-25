079c726ea4428d2b086890ff3a23ef33920b4208
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-10-25 02:45:04 -0500

    Delete device.mk

--- device.mk
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-# Copyright (C) 2019-Present A-Team Digital Solutions
-#
-
-LOCAL_PATH := device/umx/U693CL
-
