96f210d7d41a1dc8bca72b3c5ca2da407d65c598
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-11-10 19:34:58 -0600

    Fix MTP, Fix Decryption

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2019-Present A-Team Digital Solutions
+#
+
+PRODUCT_MAKEFILES := \
+     $(LOCAL_DIR)/omni_U693CL.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_U693CL-user \
+    omni_U693CL-userdebug \
+    omni_U693CL-eng
---
+++ device.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2019-Present A-Team Digital Solutions
+#
+
+LOCAL_PATH := device/umx/U693CL
+
+# QCOM Standard Decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
